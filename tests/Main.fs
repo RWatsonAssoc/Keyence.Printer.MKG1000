86cd508065d5cd75b0a2f09b54c5c8dd9c86c1c0
Russell Dillin <rdillin@rwatsonassoc.com>
2022-10-25 17:23:35 -0700

    Copyright file header for Main.fs, Tests.fs

--- tests/Main.fs
+++ tests/Main.fs
@@ -1,4 +1,14 @@
-﻿module Keyence.Printer.MKG1000.Tests
+﻿// <copyright file="Main.fs" company="R. Watson &amp; Associates, Inc.">
+// Copyright (c) 2022 R. Watson &amp; Associates, Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+// </copyright>
+// <author>Russell Dillin</author>
+// <summary>Entry point for tests</summary>
+
+module Keyence.Printer.MKG1000.Tests
 
 open Expecto
 
